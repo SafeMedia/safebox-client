b53feb1b41f0e8033aebd291b3a104757da71627
cobite <chainwizard@proton.me>
2025-07-10 16:05:22 +0100

    update esonner

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import RootLayout from "./components/root-layout";
 import AppRoutes from "./routes";
 import { Toaster } from "sonner";
-import "sonner/dist/styles.css";
 
 import { StorageProvider } from "./providers/storage-provider";
 
